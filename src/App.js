0d66694597fefe1989c7955156cd8b5a8130b314
anmolsingh0001 <115335691+anmolsingh0001@users.noreply.github.com>
2022-12-28 16:28:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import Carousel from "./components/Carousel";
+import "./App.css";
+import Carouselhoks from "./components/Carouselhoks";
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Carouselhoks />
+      </div>
+    );
+  }
+}
